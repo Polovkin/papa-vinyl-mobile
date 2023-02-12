152ee4fdab426a75ebadf55468de45636d5814be
mikle <zimboroda@gmail.com>
2023-02-12 21:41:59 +0200

    feat(project): add i18n

--- types/vendors/product.types.ts
+++ types/vendors/product.types.ts
@@ -1,31 +1,5 @@
 import type {Product} from 'papa-vinyl-types';
 
-export enum PRODUCT_STATUS {
-  AVAILABLE = 'AVAILABLE',
-  UNAVAILABLE = 'UNAVAILABLE',
-  RESERVED = 'RESERVED',
-}
-
-export enum PRODUCT_FIELDS {
-  id = 'id',
-  name = 'name',
-  url = 'url',
-  metaDescription = 'metaDescription',
-  metaTitle = 'metaTitle',
-  tag = 'tag',
-  description = 'description',
-  price = 'price',
-  purchasePrice = 'purchasePrice',
-  quantity = 'quantity',
-  catalogNumber = 'catalogNumber',
-  receiptDate = 'receiptDate',
-  year = 'year',
-  createdAt = 'createdAt',
-  status = 'status',
-  categoryId = 'categoryId',
-  thumbnail = 'thumbnail',
-}
-
 export type IProduct = Product.Interface;
 
 export type ProductRequest = Omit<
